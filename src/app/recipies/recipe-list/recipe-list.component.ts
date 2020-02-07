e50acbd456884d1744393e9d6cd4c1cd6ef3a909
discussion4all <discussion4all@yahoo.com>
2020-02-07 16:15:06 +0530

    "Created Recipe Model, And created code for showing a recipe in recipe-list.component.html"

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  // tslint:disable-next-line:max-line-length
+  recipies: Recipe[] = [new Recipe('Test Recipe', 'This is simply a test recipe', `https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg`)];
   constructor() { }
 
   ngOnInit() {
--- src/app/recipies/recipies.component.ts
+++ src/app/recipies/recipies.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipies.component.css']
 })
 export class RecipiesComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
