5e308abe0bf58811cc51281715041483e0c574cd
discussion4all <discussion4all@yahoo.com>
2020-02-10 09:49:19 +0530

    "Fixed bug after recipe service"

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -14,6 +14,7 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+    console.log(this.recipes);
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipies/recipies.component.ts
+++ src/app/recipies/recipies.component.ts
@@ -10,7 +10,7 @@ import {RecipeService} from './recipe.service';
 })
 export class RecipiesComponent implements OnInit {
   selectedRecipe: Recipe;
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
   ngOnInit() {
   }
 
